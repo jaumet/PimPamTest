c15e370f0eb60763473a8718b2bc4ecf6aa66036
Jaume <jnualart@uoc.edu>
2026-07-30 03:04:59 +0200

    Redissenyd e portada!

--- exercises/tests.py
+++ exercises/tests.py
@@ -1,6 +1,7 @@
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TestCase
+from django.urls import reverse
 
-from .models import normalize_answer
+from .models import Category, Exercise, normalize_answer
 
 
 class NormalizeAnswerTests(SimpleTestCase):
@@ -13,3 +14,28 @@ class NormalizeAnswerTests(SimpleTestCase):
 
     def test_normalizes_decimal_separators_and_trailing_zeroes(self):
         self.assertEqual(normalize_answer("3,50 €"), normalize_answer("3.5€"))
+
+
+class AnonymousHomeTests(TestCase):
+    def setUp(self):
+        category = Category.objects.create(name="Matemàtiques", slug="matematiques")
+        self.exercise = Exercise.objects.create(
+            title="Sumes senzilles",
+            statement="Calcula el resultat.",
+            category=category,
+            level=0,
+            kind=Exercise.ExerciseKind.MULTIPLE_CHOICE,
+        )
+
+    def test_home_invites_practice_without_registration(self):
+        response = self.client.get(reverse("home"))
+
+        self.assertContains(response, "Comença a practicar")
+        self.assertContains(response, "Pots començar sense registre")
+        self.assertContains(response, self.exercise.get_absolute_url())
+
+    def test_home_presents_identification_as_progress_saving_option(self):
+        response = self.client.get(reverse("home"))
+
+        self.assertContains(response, "desa el teu progrés")
+        self.assertContains(response, "La identificació només serveix per recordar el progrés")
